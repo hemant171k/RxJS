fbb6e6ac67483bb3c1efb8815f39b3ba3e13cd5a
hemant171k <hemant171k@gmail.com>
2022-08-04 16:10:22 +0530

    gitignore added

--- src/app/components/concat-merge/concat-merge.component.ts
+++ src/app/components/concat-merge/concat-merge.component.ts
@@ -1,14 +1,29 @@
-import { Component, OnInit } from '@angular/core';
-import { concat, interval, map, Subscription, take } from 'rxjs';
-import { DatashareService } from 'src/app/datashare.service';
+import { Component, OnInit } from "@angular/core";
+import {
+  concat,
+  from,
+  interval,
+  map,
+  merge,
+  mergeMap,
+  of,
+  Subscription,
+  take,
+  toArray,
+} from "rxjs";
+import { DatashareService } from "src/app/datashare.service";
 
 @Component({
-  selector: 'app-concat-merge',
-  templateUrl: './concat-merge.component.html',
-  styleUrls: ['./concat-merge.component.scss'],
+  selector: "app-concat-merge",
+  templateUrl: "./concat-merge.component.html",
+  styleUrls: ["./concat-merge.component.scss"],
 })
 export class ConcatMergeComponent implements OnInit {
   items: any = [];
+  concatItems: any = [];
+  sourceTechFeeds: any = [];
+  sourceComedyFeeds: any = [];
+  sourceNewsFeeds: any = [];
 
   subscribeMode2: boolean = true;
 
@@ -21,21 +36,38 @@ export class ConcatMergeComponent implements OnInit {
 
   ngOnInit(): void {
     const sourceTech = interval(1000).pipe(
-      map((v) => `video #${v}`),
+      map((v) => `Video #${v + 1}`),
       take(5)
     );
     const sourceComedy = interval(1000).pipe(
-      map((v) => `video #${v}`),
-      take(5)
+      map((v) => `Comedy #${v + 1}`),
+      take(6)
     );
     const sourceNews = interval(1000).pipe(
-      map((v) => `video #${v}`),
-      take(5)
+      map((v) => `News #${v + 1}`),
+      take(3)
     );
+
+    //sourceTech.subscribe((res) => (this.sourceTechFeeds = res));
+    // sourceComedy.subscribe((res) => (this.sourceComedyFeeds = res));
+    // sourceNews.subscribe((res) => (this.sourceNewsFeeds = res));
+
     let concatObs = concat(sourceTech, sourceComedy, sourceNews);
-    concatObs.subscribe((res) => (this.items = res));
+    // concatObs.subscribe((res) => this.items.push(res));
+
+    let concatObsMerge = merge(sourceTech, sourceComedy, sourceNews);
+    // concatObsMerge.subscribe((res) => this.concatItems.push(res));  // commented but its wokring
+
+    //********################################ MERGE MAP ******************************** */
+    let source = from(["Tech", "Comdedy", "News"]);
+    source
+      .pipe(mergeMap((res) => this.getData(res)))
+      .subscribe((res) => console.log(res));
   }
 
+  getData(data: any): any {
+    return of(`${data} Video Uploaded Successfully`);
+  }
   //show hide add video button
   toggleButton(): void {
     const broadcastVideo = interval(1000);
--- src/app/datashare.service.ts
+++ src/app/datashare.service.ts
@@ -1,12 +1,19 @@
-import { Injectable } from '@angular/core';
-import { BehaviorSubject, ReplaySubject, Subject } from 'rxjs';
+import { Injectable } from "@angular/core";
+import { BehaviorSubject, ReplaySubject, Subject } from "rxjs";
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: "root",
 })
 export class DatashareService {
   exclusive = new Subject<boolean>();
-  username = new BehaviorSubject<string>('Hemant');
+  username = new BehaviorSubject<string>("Hemant");
   videoemit = new ReplaySubject<string>(5, 3000);
   constructor() {}
+
+  print(val: any, cotainerId: string) {
+    let el = document.createElement("li");
+    el.innerHTML = val;
+
+    document.getElementById(cotainerId)?.appendChild(el);
+  }
 }
